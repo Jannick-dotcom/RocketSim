60fb9ef57266ea90d48316c2ea56a1964ec63deb
Jannick <jannick9743@gmail.com>
2022-05-14 15:19:23 +0200

    edit plot to generate clearer result

--- src/plot.py
+++ src/plot.py
@@ -7,16 +7,18 @@ yalt = []
 yspd = []
 
 for line in file:
-    yalt.append(float(line.split(",")[0]))
-    yspd.append(float(line.split(",")[1])*-1)
+    x.append(float(line.split(",")[0]))
+    yalt.append(float(line.split(",")[1]))
+    yspd.append(float(line.split(",")[2])*-1)
 
-x = np.linspace(0, len(yspd) * 0.001, len(yspd))
-dx = x[1]-x[0]
-dydx = np.gradient(yspd, dx)
+# x = np.linspace(0, len(yspd), len(yspd))
+# dx = x[1]-x[0]
+# dydx = np.gradient(yspd, dx)
 
 plt.figure("landing")
-# plt.plot(x,yalt)
+plt.plot(x,yalt, Label="alt")
 # plt.plot(x, dydx)
-plt.plot(x, yspd)
+plt.plot(x, yspd, Label="spd")
+plt.legend()
 # plt.plot(z)
 plt.show()
