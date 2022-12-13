37f6f24ec2c33696ed37161b8049fa7078c2d07a
Jannick-dotcom <64270059+Jannick-dotcom@users.noreply.github.com>
2022-12-13 14:32:23 +0000

    update build and launch script for dependencies

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-g++ -Wall src/main.cpp -pthread -Isrc/autoland -o build/a.out
+yes | sudo apt-get install libboost-all-dev
+mkdir build
+g++ -Wall src/main.cpp inc/autoland/autoland.cpp inc/vectors/vectors.cpp inc/main/*.cpp -pthread -Isrc -Iinc/main -Iinc/autoland -Iinc/vectors -o build/a.out
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+yes | sudo apt-get install libboost-all-dev
+mkdir build
 g++ -Wall src/main.cpp inc/autoland/autoland.cpp inc/vectors/vectors.cpp inc/main/*.cpp -pthread -Isrc -Iinc/main -Iinc/autoland -Iinc/vectors -o build/a.out
 ./build/a.out
