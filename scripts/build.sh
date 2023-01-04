cd0da56638af4e5ce32ab939fdfb3ce32ce9abcc
Jannick-dotcom <64270059+Jannick-dotcom@users.noreply.github.com>
2023-01-04 17:01:18 +0000

    update build and launch script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yes | sudo apt-get install libboost-all-dev
+rm -r build
 mkdir build
-rm build/a.out
 g++ -Wall src/main.cpp inc/autoland/autoland.cpp inc/vectors/vectors.cpp inc/main/*.cpp -pthread -Isrc -Iinc/main -Iinc/autoland -Iinc/vectors -o build/a.out
