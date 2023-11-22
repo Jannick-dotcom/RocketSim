bd5fe7fe3c362f96825757b4717d9581cc950938
Jannick <jannick9743@gmail.com>
2023-11-22 18:40:12 +0100

    remove parameter struct as not needed

--- inc/main/helpers.hpp
+++ inc/main/helpers.hpp
@@ -1,17 +1,8 @@
-// #ifndef helpers_hpp
-// #define helpers_hpp
 #pragma once
 
 #include <math.h>
 #include <stdint.h>
 #include "mainGame.hpp"
-struct parameter
-{
-    int id;
-    double valToVariate;
-    // struct vals *values;
-};
-// using namespace std;
 
 double findGAtAlt(struct vals *temp, double alt);
 
@@ -19,6 +10,4 @@ double rad2deg(double rad);
 
 double deg2rad(double deg);
 
-extern const std::string OutputPath;// = "/home/jannick/Desktop/outputs/";
-
-// #endif
+extern const std::string OutputPath;
